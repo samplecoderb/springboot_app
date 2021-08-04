f1f1ef20b6faf07740360ee016e53fe8a95d04a0
samplecoderb <88459073+samplecoderb@users.noreply.github.com>
2021-08-04 16:18:49 -0400

    Create RollbarConfig.java
    Configure the Rollbar object

---
+++ files/RollbarConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.rollbar.notifier.config.Config;
+import com.rollbar.notifier.config.ConfigBuilder;
+import com.rollbar.notifier.config.ConfigProvider;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+
+public class RollbarConfig implements ConfigProvider {
+
+  
+  @Override
+  public Config provide(ConfigBuilder configBuilder) {
+
+    // Set environment and (code) version as part of yuor build process
+
+    // Reference ConfigBuilder.java for all the properties you can set for Rollbar
+    return RollbarSpringConfigBuilder.withAccessToken(RollbarSetting.accessToken)
+        .environment("production")
+        .codeVersion("1.0.3") 
+        .build();
+  }
+
+  }
